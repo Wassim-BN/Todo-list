ac7bb139b059994c9e92d1cbb70932a2fde4a63e
Wassim-BN <wassimo294@gmail.com>
2026-06-30 10:46:30 +0200

    Stylized

--- src/components/DarkThemeComponent.vue
+++ src/components/DarkThemeComponent.vue
@@ -12,18 +12,20 @@ const toggleDark = useToggle(isDark)
 
 <template>
     <!-- Button to toggle dark mode - top right corner -->
-    <button title="Toggle dark mode 🌓"
-    class="absolute right-10 top-12 px-2 py-1 rounded active:bg-gray-300 cursor-pointer transition-colors duration-300 ease-in-out"
-    :class="
-    isDark
-        ? 'bg-yellow-600 hover:bg-yellow-700 active:bg-yellow-300 shadow shadow-white hover:-translate-y-1 duration-150 ease-in-out cursor-pointer'
-        : 'bg-gray-600 hover:bg-gray-700 active:bg-gray-300 shadow shadow-black hover:-translate-y-1 duration-150 ease-in-out cursor-pointer'
-    "
-    @click="toggleDark()">
-    <!-- Icons -->
-    <PhSun v-if="isDark" class="w-6 h-6" />
-    <PhMoon v-else class="w-6 h-6 text-white" />
-    </button>
+    <div class="absolute right-10 top-12 hover:-translate-y-1 duration-300 ease-in-out">
+        <button title="Toggle dark mode 🌓"
+        class="px-2 py-1 rounded active:bg-gray-300 cursor-pointer transition-colors duration-300 ease-in-out"
+        :class="
+        isDark
+            ? 'bg-yellow-600 hover:bg-yellow-700 active:bg-yellow-300 shadow shadow-white cursor-pointer'
+            : 'bg-gray-600 hover:bg-gray-700 active:bg-gray-300 shadow shadow-black cursor-pointer'
+        "
+        @click="toggleDark()">
+        <!-- Icons -->
+        <PhSun v-if="isDark" class="w-6 h-6" />
+        <PhMoon v-else class="w-6 h-6 text-white" />
+        </button>
+    </div>
     <TitleComponent :class="isDark ? 'bg-yellow-400 text-black' : 'bg-blue-800 text-white'" />
 </template>
 
